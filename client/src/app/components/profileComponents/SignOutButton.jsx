8324d018deb5e550fa91ff36ae8b7edcaa445fc1
Hayder <43295048+Hayder000@users.noreply.github.com>
2024-03-20 03:27:05 +0300

    Update SignOutButton.jsx

--- client/src/app/components/profileComponents/SignOutButton.jsx
+++ client/src/app/components/profileComponents/SignOutButton.jsx
@@ -1,12 +1,9 @@
 "use client";
 import React from "react";
 import axios from "axios";
-import { useRouter } from "next/navigation";
 import { BASE_URL } from "@/app/utls/constants";
 
 const SignOutButton = () => {
-  const router = useRouter();
-
   const handleLogout = async () => {
     try {
       const response = await axios.post(`${BASE_URL}/user/logout`, {});
@@ -17,19 +14,20 @@ const SignOutButton = () => {
 
       document.cookie =
         "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-      router.push("/signin");
     } catch (err) {
       console.error(err);
     }
   };
 
   return (
-    <button
-      className="px-4 py-2 bg-red-500 text-white border-2 border-red-600 hover:bg-red-600 duration-300 rounded-r-full"
-      onClick={handleLogout}
-    >
-      Sign Out
-    </button>
+    <a href="/signin">
+      <button
+        className="px-4 py-2 bg-red-500 text-white border-2 border-red-600 hover:bg-red-600 duration-300 rounded-r-full"
+        onClick={handleLogout}
+      >
+        Sign Out
+      </button>
+    </a>
   );
 };
 
